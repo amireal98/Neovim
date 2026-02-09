ea5c10abf239b7ddcd1ce9575dad20778e149eaa
amireal98 <amireal98@proton.me>
2026-02-08 19:18:56 -0600

    minor changes

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -1,11 +1,5 @@
 return {
   {
-    "sainnhe/sonokai",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.sonokai_enable_italic = true
-      vim.cmd.colorscheme('sonokai')
-    end
+    -- nothing here for now
   },
 }
