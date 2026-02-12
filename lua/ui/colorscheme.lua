e10f0f15e4936db692000328972fd45500da7961
amireal98 <amireal98@proton.me>
2026-02-11 20:28:39 -0600

    added zenbones colorscheme

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -1,5 +1,12 @@
 return {
   {
-    -- nothing here for now
+    "zenbones-theme/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.zenbones_darken_comments = 45
+      vim.cmd.colorscheme('zenbones')
+    end,
   },
 }
