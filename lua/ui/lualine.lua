5293646d5aa9ae8f339f482be48073edb3040986
amireal98 <amireal98@proton.me>
2026-02-11 20:28:45 -0600

    minor changes

--- lua/ui/lualine.lua
+++ lua/ui/lualine.lua
@@ -7,7 +7,7 @@ return {
         options = {
           disabled_filetypes = { statusline = { "snacks_dashboard" } },
           icons_enabled = false,
-          theme = "auto",
+          theme = "zenbones",
           section_separators   = { left = '', right = '' },
           component_separators = { left = '', right = '' },
           refresh = {
